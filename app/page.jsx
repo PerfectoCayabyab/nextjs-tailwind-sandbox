622a5b1955e5c34ea872b955c0dd3f52099c33aa
Perfecto Cayabyab <perfectocayabyab@gmail.com>
2024-08-03 02:12:27 +0800

    refact: reset entire project

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque, ratione! Accusamus iure architecto dignissimos excepturi eaque fugiat neque, distinctio natus. Voluptatem minus, voluptatibus velit blanditiis asperiores aliquid ab animi repellendus.</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque,
+        ratione! Accusamus iure architecto dignissimos excepturi eaque fugiat
+        neque, distinctio natus. Voluptatem minus, voluptatibus velit blanditiis
+        asperiores aliquid ab animi repellendus.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
