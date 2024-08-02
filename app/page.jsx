8700ebeb3ee4969efddf56c237f790249c7ce9a4
Perfecto Cayabyab <perfectocayabyab@gmail.com>
2024-08-03 03:37:02 +0800

    feat: Create Basic Structure

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,6 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque,
-        ratione! Accusamus iure architecto dignissimos excepturi eaque fugiat
-        neque, distinctio natus. Voluptatem minus, voluptatibus velit blanditiis
-        asperiores aliquid ab animi repellendus.
-      </p>
     </div>
   );
 };
